b7c12a765f57523c02b8d47ff6023a2ad287e0a1
Puggen <112873174+Puggen@users.noreply.github.com>
2023-12-01 10:28:58 +0100

    Add files via upload

---
+++ gik339--Grupp-2--labb1/script.js
@@ -0,0 +1,35 @@
+
+const myCheckbox = document.getElementById('divStyle');
+
+const myTextfield = document.getElementsByClassName('textfield');
+
+const myButton = document.querySelector('#btn');
+
+const myDiv = document.getElementById('div');
+
+myButton.addEventListener('click', function() {
+    console.log(myButton);
+    myDiv.remove();
+
+});
+
+myCheckbox.addEventListener('click', function() { 
+    console.log(myCheckbox);
+    getvalue();
+    
+});
+
+
+for (let i = 0; i < myTextfield.length; i++) {
+    myTextfield[i].addEventListener('input', function() {
+    var input = myTextfield[1].value;
+    myDiv.innerHTML=input;
+    });
+}
+
+
+function getvalue(){
+    var inputValue = myTextfield[0].value;
+    myDiv.style.backgroundColor = inputValue;
+}  
+ 
